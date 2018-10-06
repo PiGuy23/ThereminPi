c5403344242ff87395763e1056dbc448f4c12dd8
Adam Padilla <apadilla222@gmail.com>
2018-10-06 12:27:49 -0600

    Theses dont work yesy

---
+++ psonic_Sensors_test.py
@@ -0,0 +1,44 @@
+from gpiozero import DistanceSensor
+from time import sleep
+from psonic import *
+
+
+sensor = DistanceSensor(echo=17, trigger=4)
+
+
+val = .1
+nval = .1
+while True:
+    if(sensor.distance != 1.0) :
+        nval = (sensor.distance)
+        
+        #pitch = round(sensor.distance * 100 + 30)
+        #if (pitch <= 80) :
+            #sender.send_message('/play_this', pitch)
+        while (((nval - val) > 0.1) or ((val - nval) > 0.1)):
+            if ((nval - val) > 0.1) :
+                nval = nval - 0.1
+                
+            if ((val - nval) > .1) :
+                nval = nval + 0.1
+                
+        
+        pitch = round(val * 100 + 30)
+        if (pitch <= 80) :
+            play(pitch)
+            print(pitch)
+            val = nval
+        sleep(1)
+        
+        
+    else :
+        
+        pitch = round(val * 100 + 30)
+        play(pitch)
+        print(pitch)
+        sleep(1)
+        
+        
+    
+        
+    
--- psonic_test.py
+++ psonic_test.py
@@ -1,1 +1,20 @@
 #A Test of the Python-sonic library
+
+from psonic import *
+
+from threading import Thread
+
+def my_loop():
+  play(70)
+  sleep(1)
+
+def looper():
+  while True:
+    my_loop()
+
+looper_thread = Thread(name='looper', target=looper)
+
+looper_thread.start()
+
+input("Press Enter to continue...")
+
