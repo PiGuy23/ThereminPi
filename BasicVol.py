0887f9682a7c7e5e19b10b809ec27fc9011a6b94
Adam Padilla <apadilla222@gmail.com>
2018-11-03 17:05:11 -0600

    Organized and added BasicVol

---
+++ BasicVol.py
@@ -0,0 +1,43 @@
+import alsaaudio
+from gpiozero import DistanceSensor
+from time import sleep
+
+sensor3 = DistanceSensor(echo=22, trigger=11)
+m = alsaaudio.Mixer('PCM')
+
+
+val= .5
+nval = .5
+
+while True:
+    if(sensor3.distance < 1) :
+        nval = (sensor3.distance)
+        
+        while (((nval - val) > 0.1) or ((val - nval) > 0.1)):
+            if ((nval - val) > 0.1) :
+                nval = nval - 0.1
+                
+            if ((val - nval) > .1) :
+                nval = nval + 0.1
+                
+        
+        vol = round(((val) * 100) + 30)
+        if (vol > 100) :
+            vol = 100
+        m.setvolume(vol)
+        
+            
+        val = nval
+        sleep(.01)
+        
+        
+    else :
+        
+        vol = round(((val) * 100) + 30)
+        if (vol > 100) :
+            vol = 100
+        m.setvolume(vol)
+        
+        sleep(.01)
+
+
