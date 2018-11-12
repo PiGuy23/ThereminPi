40ea93d30c05159f78275b946291587c67c2fd93
Adam Padilla <apadilla222@gmail.com>
2018-11-11 18:29:33 -0700

    added two to varibles to distinguish form Notes

--- RunAvgVol.py
+++ RunAvgVol.py
@@ -4,34 +4,34 @@ from gpiozero import DistanceSensor
 from time import sleep
 
 sensor = DistanceSensor(echo=22, trigger=11)
-m = alsaaudio.Mixer('PCM')
+m = alsaaudio.Mix2er('PCM')
 
 
-vals = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+vals2 = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
 
-d = 0.5
+d2 = 0.5
 
 while True:
-    i = 0
-    while i < 8:
-        vals[i] = vals[i+1]
-        i = i+1
+    i2 = 0
+    while i2 < 8:
+        vals2[i2] = vals2[i2+1]
+        i2 = i2+1
     
     
-    x = (sensor.distance)
-    if x == 1.0:
-        vals[8] = d
+    x2 = (sensor.distance)
+    if x2 == 1.0:
+        vals2[8] = d2
     else:
-        vals[8] = x
-        d = x
+        vals2[8] = x2
+        d2 = x2
     
-    j = 0
-    for y in vals:
-        j = j + y
+    j2 = 0
+    for y2 in vals2:
+        j2 = j2 + y2
         
-    avg = j/4
+    avg2 = j/4
     
-    vol = round(((avg) * 100) + 30)
+    vol = round(((avg2) * 100) + 30)
     if (vol > 100) :
         vol = 100
     print(vol)
