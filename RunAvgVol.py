7c4470986c8ed4a3523bc05c2c47fd1f72f7778b
Adam Padilla <apadilla222@gmail.com>
2018-11-13 19:19:05 -0700

    Able to easily change array size

--- RunAvgVol.py
+++ RunAvgVol.py
@@ -3,36 +3,38 @@ import alsaaudio
 from gpiozero import DistanceSensor
 from time import sleep
  
-sensor = DistanceSensor(echo=24, trigger=25)
+sensor = DistanceSensor(echo=22, trigger=11)
 m = alsaaudio.Mixer('PCM')
  
  
-vals = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+vals = [0.5] * 15
 
 d = 0.5
 
 while True:
+    k = len(vals) -1
     i = 0
-    while i < 8:
+    while i < k:
         vals[i] = vals[i+1]
         i = i+1
     
     
     x = (sensor.distance)
     if x == 1.0:
-        vals[8] = d
+        vals[k] = d
     else:
-        vals[8] = x
+        vals[k] = x
         d = x
     
     j = 0
     for y in vals:
         j = j + y
         
-    avg = j/4
+    avg = j/(k+1)
     
     vol = round(((avg) * 100) + 30)
     if (vol > 100) :
         vol = 100
     m.setvolume(vol)
-    sleep(0.08)
+    
+    sleep(0.005)
