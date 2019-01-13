86b0208b7087f4c5c0ab2a4a9df66c06fcd71404
Adam Padilla <apadilla222@gmail.com>
2019-01-12 18:18:34 -0700

    Testing

--- Testing/SynthSwitch.py
+++ Testing/SynthSwitch.py
@@ -1,20 +1,22 @@
 #WorksPrettyWell
-
 from gpiozero import DistanceSensor
 from time import sleep
 from psonic import *
 
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
+
+
+
  # Import Raspberry Pi GPIO library
 
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 #GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 sender = udp_client.SimpleUDPClient('127.0.0.1', 4559)
-if GPIO.input(40) == GPIO.HIGH:
+if GPIO.input(21) == GPIO.HIGH:
     code = """
     use_synth :saw
     s = play 60, release: 30, note_slide: 0.1
--- Testing/gpiointest.py
+++ Testing/gpiointest.py
@@ -1,14 +1,16 @@
+
+
 from time import sleep
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # Set pin 21 to be an input pin and set initial value to be pulled low (off)
 print("setup done")
 
 test = "Hello"
 while True: # Run forever
-    if GPIO.input(40) == GPIO.HIGH:
+    if GPIO.input(21) == GPIO.HIGH:
         test = "Yes"
         
     else :
