58a0ba319375346eae55cb67e767d3808701dc81
Adam Padilla <apadilla222@gmail.com>
2019-01-12 20:23:56 -0700

    testing

--- BetterRunAvgNotes.py
+++ BetterRunAvgNotes.py
@@ -27,29 +27,30 @@ run(code)
 sensor = DistanceSensor(echo=24, trigger=25)
 
 
-vals = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+vals = [0.5] * 3
 
 d = 0.5
 sender.send_message('/play_this', 60)
 while True:
+    k = len(vals) -1
     i = 0
-    while i < 8:
+    while i < k:
         vals[i] = vals[i+1]
         i = i+1
     
     
     x = (sensor.distance)
     if x == 1.0:
-        vals[8] = d
+        vals[k] = d
     else:
-        vals[8] = x
+        vals[k] = x
         d = x
     
     j = 0
     for y in vals:
         j = j + y
         
-    avg = j/4
+    avg = j/(k+1)
     
     pitch = round(avg * 100 + 30)
     if (pitch <= 150) :
